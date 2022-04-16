21b14385682d68cb34a53bd1f28d06530dc72207
Ryan Quellhorst <ryan@deeplens.ai>
2022-04-16 15:15:47 -0400

    added 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-app>
+        <Layout>
+            <v-content class="text-center">
+                <div class="headline">
+                    404
+                </div>
+                <div class="subheading">
+                    An app for a testing conference with a bug? Or maybe you were expoloratory testing. Either way you're in the wrong place. Go back before someone sees you!!!
+                </div>
+            </v-content>
+        </Layout>
+    </v-app>
+</template>
+
+<script>
+import IndexNavbar from "@/components/IndexNavbar";
+
+export default {
+  components: {
+    IndexNavbar
+  }
+};
+</script>
