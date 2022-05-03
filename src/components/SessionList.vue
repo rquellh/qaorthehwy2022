b761fe49661899d31dacb749cfc72b97783b6ef4
Ryan Quellhorst <ryan@deeplens.ai>
2022-05-03 15:40:18 -0400

    changes as of 05-03

--- src/components/SessionList.vue
+++ src/components/SessionList.vue
@@ -3,27 +3,37 @@
     <v-layout row wrap fluid>
       <v-flex>
         <v-list three-line>
-          <v-subheader class="title font-weight-bold pa-0">{{sessions.edges[0].node.time}}</v-subheader>
+          <v-subheader class="title font-weight-bold pa-0">{{
+            sessions.edges[0].node.time
+          }}</v-subheader>
           <div
-            v-show="favorites.sessions[indexOf(session.node.speaker)].saved || !favorites.filterall"
+            v-show="
+              favorites.sessions[indexOf(session.node.speaker)].saved ||
+              !favorites.filterall
+            "
             v-for="(session, index) in sessions.edges"
             :key="index"
           >
-            <v-list-item :class="timeFiltered" :href="`./${removeSpaces(session.node.speaker)}`">
+            <v-list-item
+              :class="timeFiltered"
+              :href="`./${removeSpaces(session.node.speaker)}`"
+            >
               <v-list-item-content>
-                <v-list-item-title>{{session.node.title}}</v-list-item-title>
+                <v-list-item-title>{{ session.node.title }}</v-list-item-title>
                 <v-list-item-subtitle>
                   <v-layout ma-0 pa-0 wrap>
                     <v-flex xs12 sm6>
-                      {{session.node.speaker}}
-                      <span
-                        v-if="session.node.speaker2"
-                      >& {{session.node.speaker2}}</span>
+                      {{ session.node.speaker }}
+                      <span v-if="session.node.speaker2"
+                        >& {{ session.node.speaker2 }}</span
+                      >
                     </v-flex>
                     <v-flex xs12 sm6>
-                      <v-icon small :class="roomFiltered([index])">$circlesolid</v-icon>
-                      {{session.node.room}}
-                      <span class="pl-2">({{findFloor(index)}})</span>
+                      <v-icon small :class="roomFiltered([index])"
+                        >$circlesolid</v-icon
+                      >
+                      {{ session.node.room }}
+                      <span class="pl-2">({{ findFloor(index) }})</span>
                     </v-flex>
                   </v-layout>
                 </v-list-item-subtitle>
@@ -33,13 +43,19 @@
                   icon
                   ripple
                   :title="`${session.node.speaker} Like Button`"
-                  @click.prevent.stop="favorites.sessions[indexOf(session.node.speaker)].saved = !favorites.sessions[indexOf(session.node.speaker)].saved"
+                  @click.prevent.stop="
+                    favorites.sessions[indexOf(session.node.speaker)].saved =
+                      !favorites.sessions[indexOf(session.node.speaker)].saved
+                  "
                 >
                   <v-icon
-                    v-if="favorites.sessions[indexOf(session.node.speaker)].saved"
+                    v-if="
+                      favorites.sessions[indexOf(session.node.speaker)].saved
+                    "
                     small
                     color="amber"
-                  >$starsolid</v-icon>
+                    >$starsolid</v-icon
+                  >
                   <v-icon v-else small color="grey">$staroutline</v-icon>
                 </v-btn>
               </v-list-item-action>
@@ -56,45 +72,52 @@
 <script>
 export default {
   props: ["sessions", "favorites"],
-  data: function() {
+  data: function () {
     return {
       timeFiltered: `t${this.sessions.edges[0].node.time.replace(
         /:|-| /g,
         ""
       )}`,
-      filter: false
+      filter: false,
     };
   },
   methods: {
-    indexOf: function(speakerName) {
+    indexOf: function (speakerName) {
       var indexPos = this.$props.favorites.sessions
-        .map(function(instance) {
+        .map(function (instance) {
           return instance.speaker;
         })
         .indexOf(speakerName);
       return indexPos;
     },
-    roomFiltered: function(index) {
+    roomFiltered: function (index) {
       return this.sessions.edges[index].node.room
         .replace(/:|-| |&/g, "")
         .toLowerCase();
     },
-    removeSpaces: function(text) {
+    removeSpaces: function (text) {
       return text.replace(/\s/g, "");
-    }, 
-    findFloor: function(room) {
-      var floorfilter = this.roomFiltered(room)
-      if (floorfilter == "greathall12" || floorfilter == "greathall3" || floorfilter == "usbanktheater") {
-        return "1st Floor"
-      } else if (floorfilter == "ballroom" || floorfilter == "studentalumniroom" ) {
-        return "2nd Floor"
-      } else if (floorfilter == "interfaithroom" || floorfilter == "cartoonroom" ) {
-        return "3rd Floor"
+    },
+    findFloor: function (room) {
+      var floorfilter = this.roomFiltered(room);
+      if (floorfilter == "greathall12" || floorfilter == "greathall3") {
+        return "1st Floor";
+      } else if (
+        floorfilter == "eastballroom" ||
+        floorfilter == "studentalumniroom" ||
+        floorfilter == "westballroom"
+      ) {
+        return "2nd Floor";
+      } else if (
+        floorfilter == "interfaithroom" ||
+        floorfilter == "cartoonroom"
+      ) {
+        return "3rd Floor";
       } else {
-        return "Error"
+        return "Error";
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -197,10 +220,10 @@ export default {
 .studentalumniroom {
   color: #ef9a9a;
 }
-.usbanktheater {
+.westballroom {
   color: #ffcc80;
 }
-.ballroom {
+.eastballroom {
   color: #80cbc4;
 }
 .right {
