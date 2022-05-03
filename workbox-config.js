124ab094268aad26783288b26f50fa33d008468f
Ryan Quellhorst <ryan@deeplens.ai>
2022-05-03 11:32:26 -0400

    changes to sw

--- workbox-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "globDirectory": "dist/",
-  "globPatterns": [
-    "**/*.{html,css,json,webp,png,js}"
-  ],
-  "swDest": "dist/sw.js",
-  "swSrc": "service-worker-src.js",
-  "injectionPoint": "injectionPoint"
-};
